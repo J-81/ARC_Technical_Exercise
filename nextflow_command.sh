7863a1fc42a3c1fcc783ade42318f81313d564fb
Jonathan Oribello <Jonathan.d.oribello@gmail.com>
2023-07-29 00:09:33 +0000

    typo: fox

--- nextflow_command.sh
+++ nextflow_command.sh
@@ -3,7 +3,7 @@ eval "$(micromamba shell hook --shell bash)" \
     && nextflow run main.nf \
         --bbduk_adapters /opt/conda/envs/main/bin/bbmap/resources/adapters.fa \
         --reads_dir test_data \
-/workspace/ARC_Technical_Exercise/modules        --meta_sample_name ARC_TEST_SAMPLE \
+        --meta_sample_name ARC_TEST_SAMPLE \
         --number_genome_representatives 4 \
         --sample_count_alignments 10000 \
         --output final_test
